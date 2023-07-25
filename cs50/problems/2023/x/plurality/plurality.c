b9eb9f587056fe650cacab53728073b0e0f00279
betocostadev <robertomarx@gmail.com>
2023-07-25 09:43:54 -0300

    added plurality

--- cs50/problems/2023/x/plurality/plurality.c
+++ cs50/problems/2023/x/plurality/plurality.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 // Max number of candidates
 #define MAX 9
@@ -65,13 +66,41 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-  // TODO
+  // Check if the candidate exists and add a vote to he/she if so
+  for (int i = 0; i < candidate_count; i++)
+  {
+    if (strcasecmp(name, candidates[i].name) == 0)
+    {
+      candidates[i].votes++;
+      return true;
+    }
+  }
   return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-  // TODO
+  int max_votes = -1;
+
+  // Check the entire array and get the max votes
+  for (int i = 0; i < candidate_count + 1; i++)
+  {
+    if (candidates[i].votes >= max_votes)
+    {
+      max_votes = candidates[i].votes;
+    }
+  }
+
+  // Print the winners names
+  for (int i = 0; i < candidate_count; i++)
+  {
+    if (candidates[i].votes >= max_votes)
+    {
+      printf("%s", candidates[i].name);
+      printf("\n");
+    }
+  }
+
   return;
 }
